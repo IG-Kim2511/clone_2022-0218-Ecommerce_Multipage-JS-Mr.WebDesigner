c8d971917f549370aeee8b43b868db9cbb48b51f
IG <iikim2511@gmail.com>
2022-02-19 14:10:07 -0500

    init

---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+
+/* 🍀side bar */
+
+let sideBar = document.querySelector('.side-bar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    sideBar.classList.toggle('active');
+}
+
+document.querySelector('#close-side-bar').onclick = () =>{
+    sideBar.classList.remove('active');
+}
+
+
+/* 🍀search bar show at media query phone mode */
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    sideBar.classList.remove('active');
+    searchForm.classList.remove('active');
+};
+
+
+/* 🍀slide show js framework - swiper */
+document.querySelectorAll('.accordion-container .accordion').forEach(accordion =>{
+    accordion.onclick = () =>{
+        accordion.classList.toggle('active');
+    }
+});
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        450: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
